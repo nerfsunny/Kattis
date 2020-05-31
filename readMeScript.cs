068753c273f329ac20b3dffd7018809814397eb1
nerfsunny <nerfsunny@gmail.com>
2020-05-31 15:22:31 -0400

    Updated file

--- readMeScript.cs
+++ readMeScript.cs
@@ -18,10 +18,11 @@ public class readMeScript {
 
 		System.IO.StreamWriter outputFile = new System.IO.StreamWriter("ReadMe.md");
 
-		outputFile.WriteLine("This repository contains my attempt at solving programming problems I find online. Below there is a table that contains the problem name, the status of the problem and what language(s) it is completed in.\n");
+		outputFile.WriteLine("This repository contains my attempt at solving programming problems I find online. Below there is a table that contains the problem name, the status of the problem, where the problem was found and what language(s) it is completed in.\n");
 		outputFile.WriteLine(header);
 		outputFile.WriteLine("------------ | ------------ | ------------ | ------------");
 
+		//Sorts the lines in alphabetical order
 		lines.Sort();
 
 		foreach(string x in lines)
