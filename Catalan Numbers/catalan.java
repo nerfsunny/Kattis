fa0044ba1d283894035052bef05192141fb48845
nerfsunny <nerfsunny@gmail.com>
2020-06-24 19:01:46 -0400

    Current progress on problem. Passing 1/2 tests on Kattis. Getting 'Time Limit Exceeded' on the second test

---
+++ Catalan Numbers/catalan.java
@@ -0,0 +1,32 @@
+import java.lang.*;
+import java.util.*;
+import java.math.*;
+
+public class catalan {
+	public static BigInteger calculateFactorial(int number) {
+		BigInteger value = BigInteger.ONE;
+
+		for(int i = 2; i <= number; i++)
+			value = value.multiply(BigInteger.valueOf(i));
+
+		return value;
+	}
+
+	public static BigInteger calculateCatalanNumber(int number) {
+		BigInteger denominator = calculateFactorial(number).multiply(calculateFactorial(number + 1));
+		BigInteger numerator = calculateFactorial(number * 2);
+
+		return numerator.divide(denominator);
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int number, numberOfInputs = scan.nextInt();
+
+		while(scan.hasNextInt()) {
+			number = scan.nextInt();
+
+			System.out.println(calculateCatalanNumber(number));
+		}
+	}
+}
