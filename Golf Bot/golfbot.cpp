af7e5d17b444597c00d8fd787c6549b87888b031
nerfsunny <nerfsunny@gmail.com>
2020-06-24 19:06:44 -0400

    Current progress on problem

--- Golf Bot/golfbot.cpp
+++ Golf Bot/golfbot.cpp
@@ -1,68 +1,87 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <unordered_set>
 #include <algorithm>
 
 using namespace std;
 
 int main(int argc, char** argv) {
-	//vector<int> shotDistance;
-	//vector<int> courseLength;
-	set<int> shotDistance;
-	set<int> courseLength;
+    //vector<int> shotDistance;
+    //vector<int> courseLength;
+    //set<int> shotDistance;
+    //set<int> courseLength;
+    unordered_set<int> shotDistance;
+    unordered_set<int> courseLength;
 
-	int shotDistanceInput, courseLengthInput, input;
-	int numberOfHoles = 0;
+    int shotDistanceInput, courseLengthInput, input;
+    int numberOfHoles = 0;
 
-	cin >> shotDistanceInput;
-	//shotDistance.reserve(shotDistanceInput);
+    cin >> shotDistanceInput;
+    shotDistance.reserve(shotDistanceInput);
 
-	for(int i = 0; i < shotDistanceInput; i++) {
-		cin >> input;
-		//shotDistance.emplace_back(input);
-		shotDistance.insert(input);
-	}
+    for(int i = 0; i < shotDistanceInput; i++) {
+        if(scanf("%u", &input) == 1)
+        	shotDistance.emplace(input);
+			//shotDistance.emplace_back(input);
+    }
 
-	cin >> courseLengthInput;
-	//courseLength.reserve(courseLengthInput);
+    cin >> courseLengthInput;
+    courseLength.reserve(courseLengthInput);
 
-	for(int i = 0; i < courseLengthInput; i++) {
-		cin >> input;
-		//courseLength.emplace_back(input);
-		courseLength.insert(input);
-	}
+    for(int i = 0; i < courseLengthInput; i++) {
+        if(scanf("%u", &input) == 1)
+        	courseLength.emplace(input);
+			//courseLength.emplace_back(input);
+    }
 
-	auto begin = shotDistance.begin();
-	auto end = shotDistance.end();
+    auto begin = shotDistance.begin();
+    auto end = shotDistance.end();
 
-	for(auto &x : courseLength) {
-		if(binary_search(begin, end, x)) {
-			numberOfHoles++;
-			continue;
-		}
+    for(auto &x : courseLength) {
+        /*if(binary_search(begin, end, x)) {
+            numberOfHoles++;
+            continue;
+        }*/
 
-		/*if(shotDistance.find(x) != end) {
-			numberOfHoles++;
-			continue;
-		}*/
+        //cout << "Course Length: " << x << endl;
 
-		for(auto &y : shotDistance) {
-			if(y > x)
-				break;
+        /*if(binary_search(begin, end, x)) {
+        	numberOfHoles++;
+        	continue;
+        }*/
 
-			if(binary_search(begin, end, (x - y))) {
-				numberOfHoles++;
-				break;
-			}
+        if(shotDistance.find(x) != end) {
+            //cout << "In if-statement 1: " << x << endl;
+            numberOfHoles++;
+            continue;
+        }
 
-			/*if(shotDistance.find(x - y) != end) {
-				numberOfHoles++;
-				break;
-			}*/
-		}
-	}
+        for(auto &y : shotDistance) {
+            /*if(y > x)
+                break;*/
 
-	cout << numberOfHoles << "\n";
+            //cout << "Shot Distance: " << x << endl;
 
-	return 0;
+            /*if(binary_search(begin, end, (x - y))) {
+                numberOfHoles++;
+                break;
+            }*/
+
+            /*if(binary_search(begin, end, (x - y))) {
+            	numberOfHoles++;
+            	break;
+            }*/
+
+            if(shotDistance.find(x - y) != end) {
+                //cout << "In if-statement 2: " << x - y << endl;
+                numberOfHoles++;
+                break;
+            }
+        }
+    }
+
+    cout << numberOfHoles << "\n";
+
+    return 0;
 }
