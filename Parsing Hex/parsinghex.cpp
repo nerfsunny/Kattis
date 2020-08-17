0bbb4b254cba299973d9c935264ec8b1e9092c0c
nerfsunny <nerfsunny@gmail.com>
2020-08-17 18:05:29 -0400

    Current progress on problem. Rewriting code to implement algorithm to find substrings faster.

--- Parsing Hex/parsinghex.cpp
+++ Parsing Hex/parsinghex.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <set>
 #include <cmath>
 #include <unordered_map>
 #include <algorithm>
@@ -34,57 +35,38 @@ unordered_map<char, int> populateSet() {
 	return charSet;
 }
 
-int main(int argc, char** argv) {
-	string line, hexNumber;
-	unordered_map<char, int> charSet = populateSet();
-
+string getHexString(string& line, const int index, const unordered_map<char, int>& charSet) {
+	string hex = line.substr(index, 2);
 	auto end = charSet.end();
 
-	while(cin >> line) {
-		while(!line.empty()) {
-			size_t index = line.find("0x");
-
-			if(index != string::npos) {
-				string hex = "0x";
-
-				for(int i = index + 2; i < line.length(); i++) {
-					if(charSet.find(line[i]) != end)
-						hex.append(line.substr(i, 1));
-					else {
-						line = line.substr(i + 1);
-						break;
-					}
-				}
-
-				hexNumber = hex.substr(2);
-				cout << hex << " " << convertFromHexToDecimal(hexNumber, charSet) << endl;
-
-				continue;
-			}
-
-			index = line.find("0X");
-
-			if(index != string::npos) {
-				string hex = "0X";
-
-				for(int i = index + 2; i < line.length(); i++) {
-					if(charSet.find(line[i]) != end)
-						hex.append(line.substr(i, 1));
-					else {
-						line = line.substr(i + 1);
-						break;
-					}
-				}
-
-				hexNumber = hex.substr(2);
-				cout << hex << " " << convertFromHexToDecimal(hexNumber, charSet) << endl;
-
-				continue;
-			}
-
-			if(index == string::npos)
-				line.clear();
+	for(int i = index + 2; i < line.length(); i++) {
+		if(charSet.find(line[i]) != end)
+			hex.append(line.substr(i, 1));
+		else {
+			line = line.substr(i + 1);
+			break;
 		}
+	}
+
+	return hex;
+}
+
+set<int> hexStringStartingIndexes(string line, unordered_map<char, int> charSet) {
+	set<int> indexes;
+
+	while(!line.empty()) {
+
+	}
+
+	return indexes;
+}
+
+int main(int argc, char** argv) {
+	string line;
+	unordered_map<char, int> charSet = populateSet();
+
+	while(cin >> line) {
+		set<int> indexes = hexStringStartingIndexes(line, charSet);
 	}
 
 	return 0;
