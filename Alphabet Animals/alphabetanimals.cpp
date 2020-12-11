e22e1b7afa780c1f26fceca77b1bcf4f972f59f1
nerfsunny <nerfsunny@gmail.com>
2020-12-10 20:40:04 -0500

    Current progress on problem. Don't currently remember what is being changed in this program

--- Alphabet Animals/alphabetanimals.cpp
+++ Alphabet Animals/alphabetanimals.cpp
@@ -12,31 +12,28 @@ int main(int argc, char** argv) {
 
     cin >> initialWord >> num;
 
-    while(cin >> word) {
-        if(potentialWords.find(word[0]) != potentialWords.end()) 
-            potentialWords.at(word[0]).push_back(word);
-        else {
-            potentialWords[word[0]].push_back(word);
-        }
-    }
+    while(cin >> word) 
+        potentialWords[word[0]].emplace_back(word);
 
     word.clear();
     char startingLetter = initialWord[initialWord.length() - 1];
     vector<string> wordList;
 
     if(potentialWords.find(startingLetter) != potentialWords.end()) {
-        wordList.reserve(potentialWords.at(startingLetter).size());
+        wordList.reserve(potentialWords[startingLetter].size());
 
-        while(potentialWords.at(startingLetter).size() > 0) {
+       while(potentialWords[startingLetter].size() > 0) {
             string temp = potentialWords.at(startingLetter).at(0);
             potentialWords.at(startingLetter).erase(potentialWords.at(startingLetter).begin());
 
-            if(potentialWords.find(temp[temp.length() - 1]) == potentialWords.end()) {
+            bool isNotInList = (potentialWords.find(temp[temp.length() - 1]) == potentialWords.end());
+
+            if(isNotInList) {
                 word = temp + "!";
                 break;
             }
 
-            if(potentialWords.find(temp[temp.length() - 1]) != potentialWords.end() && potentialWords.at(temp[temp.length() - 1]).size() == 0) {
+            if(!isNotInList && potentialWords.at(temp[temp.length() - 1]).size() == 0) {
                 word = temp + "!";
                 break;
             }
@@ -44,11 +41,8 @@ int main(int argc, char** argv) {
             wordList.push_back(temp);
         }
 
-        wordList.insert(wordList.end(), potentialWords.at(startingLetter).begin(), potentialWords.at(startingLetter).end());
-        potentialWords[startingLetter] = wordList;
-
         if(word.empty())
-            word = potentialWords.at(startingLetter).at(0);
+            word = wordList.at(0);
     }else
         word = "?";
 
