80147a0d0039575d49b3236a25739c11afb6e028
nerfsunny <nerfsunny@gmail.com>
2020-12-12 03:06:28 -0500

    Current progress on problem.

---
+++ Joyless Dream/joylessdream.java
@@ -0,0 +1,17 @@
+import java.lang.String;
+import java.util.Scanner;
+
+public class joylessdream {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numberOfInputs = Integer.parseInt(scan.nextLine());
+        String word;
+
+        for(int i = 0; i < numberOfInputs; i++) {
+            word = scan.nextLine();
+            
+        }
+
+        scan.close();
+    }
+}
