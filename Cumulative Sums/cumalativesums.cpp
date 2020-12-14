6c0cd218916f1402f5e6345d963abc0ffc670876
nerfsunny <nerfsunny@gmail.com>
2020-12-13 21:17:42 -0500

    Current progress on problem. Currently passes 1/3 tests on Kattis. Getting 'Memory Limit Exceeded' error

--- Cumulative Sums/cumalativesums.cpp
+++ Cumulative Sums/cumalativesums.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <set>
+#include <vector>
 
 using namespace std;
 using ull = unsigned long long int;
@@ -15,9 +15,33 @@ ull calculateSOD(ull num) {
     return sum;
 }
 
+ull calculateSum(vector<ull>& sequence, const ull num) {
+    ull sum = 0;
+
+    for(int i = 0; i < num; i++) 
+        sum += sequence.at(i);
+
+    return sum;
+}
+
 int main(int argc, char** argv) {
-    ull num;
-    set<ull> numberSequence;
+    ull num, numberOfTestCases, sod;
+    vector<ull> numberSequence {1};
+
+    cin >> numberOfTestCases;
+
+    for(int i = 0; i < numberOfTestCases; i++) {
+        cin >> num;
+
+        if(num > numberSequence.size()) {
+            for(ull j = numberSequence.size()-1; j < num; j++) {
+                sod = numberSequence.at(j) + calculateSOD(numberSequence.at(j));
+                numberSequence.push_back(sod);
+            }
+        }
+
+        cout << (calculateSum(numberSequence, num) % 1000000007) << endl;
+    }
 
     return 0;
 }
