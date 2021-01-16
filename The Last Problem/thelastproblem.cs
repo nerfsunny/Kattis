d124d4a316c0c71b050078fa641a3db751619e6f
nerfsunny <nerfsunny@gmail.com>
2021-01-16 02:35:45 -0500

    Successfully completed problem. Documented code.

---
+++ The Last Problem/thelastproblem.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class TheLastProblem {
+    public static void Main(string[] args) {
+        //Stores given input
+        string name = Console.ReadLine();
+
+        //Outputs specific output
+        Console.WriteLine("Thank you, "+ name + ", and farewell!");
+    }
+}
