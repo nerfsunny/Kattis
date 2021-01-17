fcdd6baa0bd6aed6b8368947971c18432dfab021
nerfsunny <nerfsunny@gmail.com>
2021-01-16 21:55:46 -0500

    Current progress on problem. Currently passes 14/54 test cases. Getting 'Time Limit Exceeded'

--- Fluortanten/fluortanten.cpp
+++ Fluortanten/fluortanten.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <limits>
+#include <climits>
 
 using namespace std;
 using ll = long long int;
@@ -17,18 +19,20 @@ ll happinessSum(vector<int>& numbers) {
 
 int main(int argc, char**) {
     int size, num, startingIndex;
-    ll highestHappiness = 0, currentHappiness;
+    ll highestHappiness = LLONG_MIN, currentHappiness;
 
     cin >> size;
 
     vector<int> line;
+    vector<ll> totalHappinessValues;
     line.reserve(size);
+    totalHappinessValues.reserve(size);
 
     for(int i = 0; i < size; i++) {
         cin >> num;
 
         if(num == 0)
-            startingIndex = 1;
+            startingIndex = i;
         
         line.push_back(num);
     }
@@ -36,13 +40,22 @@ int main(int argc, char**) {
     int currentIndex = startingIndex;
 
     do {
-        
+        if(currentIndex == 0) {
+            line.erase(line.begin());
+            line.push_back(0);
+            currentIndex = size - 1;
+        }else {
+            iter_swap(line.begin() + (currentIndex - 1), line.begin() + currentIndex);
+            currentIndex--;
+        }
 
         currentHappiness = happinessSum(line);
 
         if(currentHappiness > highestHappiness)
             highestHappiness = currentHappiness;
     }while(startingIndex != currentIndex);
+
+    cout << highestHappiness << endl;
     
     return 0;
 }
