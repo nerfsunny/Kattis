e799f0aa780a2d2691db8c7ecefc2ecf5cec1cf3
nerfsunny <nerfsunny@gmail.com>
2021-05-30 01:07:20 -0400

    Current progress on problem. Currently passes 1/2 tests. Getting 'Wrong Answer'

--- Beehives/beehives.cpp
+++ Beehives/beehives.cpp
@@ -6,43 +6,45 @@
 
 using namespace std;
 
-void printResults(map<pair<double, double>, int>& numberOfConflicts, const int noConflicts) {
+void printResults(map<pair<double, double>, bool>& producesSweetHoney) {
 	int sweet = 0, sour = 0;
 
-	for(auto& point : numberOfConflicts)
-		if(point.second == noConflicts)
+	for(auto& beehive : producesSweetHoney)
+		if(beehive.second)
 			sweet++;
 		else
 			sour++;
 
-	printf("%i sour, %i sweet", sour, sweet);
+	printf("%i sour, %i sweet\n", sour, sweet);
 }
 
-void populate(map<pair<double, double>, int>& numberOfConflicts, vector<pair<double, double>>& points, const int inputSize) {
+void populate(map<pair<double, double>, bool>& producesSweetHoney, vector<pair<double, double>>& points, const int inputSize) {
 	double xPoint, yPoint;
+	pair<double, double> point;
 
 	for(int i = 0; i < inputSize; i++) {
 		cin >> xPoint >> yPoint;
-		points.push_back(make_pair(xPoint, yPoint));
-		numberOfConflicts.insert({make_pair(xPoint, yPoint), 0});
+		point = make_pair(xPoint, yPoint);
+		points.push_back(point);
+		producesSweetHoney.insert({point, true});
 	}
 }
 
 double calculateDistance(pair<double, double>& firstPoint, pair<double, double>& secondPoint) {
-	double x = firstPoint.first + secondPoint.first, y = firstPoint.second + secondPoint.second;
-	double x2 = pow(x, 2), y2 = pow(y, 2);
+	double x = pow(firstPoint.first - secondPoint.first, 2), 
+	       y = pow(firstPoint.second - secondPoint.second, 2);
 
-	double result = pow((x2 + y2), 0.5);
-
-	return result;
+	return sqrt(x + y);
 }
 
-void calculateConflicts(map<pair<double, double>, int>& numberOfConflicts, vector<pair<double, double>>& points, const int inputSize, const double distance) {
-	for(int i = 0; i < inputSize; i++) {
-		for(int j = i + 1; j < inputSize; j++) {
-			if(calculateDistance(points.at(i), points.at(j)) > distance) {
-				numberOfConflicts[points.at(i)]++;
-				numberOfConflicts[points.at(j)]++;
+void calculateConflicts(map<pair<double, double>, bool>& producesSweetHoney, vector<pair<double, double>>& points, const int inputSize, const double distance) {
+	for(int i = 0; i < points.size(); i++) {
+		if(producesSweetHoney.at(points.at(i))) {
+			for(int j = i + 1; j < points.size(); j++) {
+				if(producesSweetHoney.at(points.at(j)) && calculateDistance(points.at(i), points.at(j)) < distance) {
+					producesSweetHoney.at(points.at(i)) = false;
+					producesSweetHoney.at(points.at(j)) = false;
+				}
 			}
 		}
 	}
@@ -50,24 +52,21 @@ void calculateConflicts(map<pair<double, double>, int>& numberOfConflicts, vecto
 
 int main(int argc, char** argv) {
 	vector<pair<double, double>> points;
-	map<pair<double, double>, int> numberOfConflicts;
+	map<pair<double, double>, bool> producesSweetHoney;
 	double distance;
 	int inputSize;
 
-	while(cin >> distance >> inputSize) {
-		if(distance == 0.0 && inputSize == 0)
-			break;
-
+	while(cin >> distance >> inputSize && distance != 0.0 && inputSize != 0) {
 		points.reserve(inputSize);
 
-		populate(numberOfConflicts, points, inputSize);
+		populate(producesSweetHoney, points, inputSize);
 
-		calculateConflicts(numberOfConflicts, points, distance, inputSize);
+		calculateConflicts(producesSweetHoney, points, inputSize, distance);
 		
-		printResults(numberOfConflicts, inputSize - 1);
+		printResults(producesSweetHoney);
 
 		points.clear();
-		numberOfConflicts.clear();
+		producesSweetHoney.clear();
 	}
 
 	return 0;
